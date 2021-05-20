d1e00836554991fd467850ef8806b23c51132b93
Corey Parker <coreyparker012@gmail.com>
2021-05-20 18:05:28 -0400

    Testing colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,50 @@
+// Grab date to put at top of page
+
+let todayDate = $('#todayDate');
+let momentDate =  moment().format('MMMM Do YYYY, h:mm a');
+
+// Append date to top of page
+
+todayDate.append(momentDate);
+
+// Get current hours
+
+let timeZ = new Date().getHours();
+
+// Test setting the time while building this after hours
+
+// let timeZ = 12;
+
+// Hours array
+
+let allHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
 // Function to change background color based on time
 
+function changeColors() {
+
+$('.todo').each(function(i) {
+
+    if (allHours[i] < timeZ) {
+        $(this).addClass('past');
+    } else if (allHours[i] === timeZ) {
+        $(this).addClass('current');
+    } else {
+        $(this).addClass('future')
+    }
+});
+}
+
+changeColors();
+
+
+
+
 // Function to save text to local storage
+
+
+
+
 
 
 
