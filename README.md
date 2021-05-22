876f544a556eda83c4b795b527bb96401bcba23c
Corey Parker <coreyparker012@gmail.com>
2021-05-22 10:03:12 -0400

    Saving page works

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Week5-WorkDay-CP
 This is Corey Parker's Week 5 homework, Work Day Scheduler.
+
+My biggest struggles/achievements were:
+
+1. Getting line 67 of js to work and correctly pull the id into the loop as a string. 
+
+2. On the css side, I had some trouble with scaling the To Do items at smaller sizes
+
+3. Lines 33 - 45 took a bit of messing with to get a concise way of writing it. I started off by trying to individually assign each item the past, current, or future id. Eventually I got it working as a loop to dynamically assign the id.
+
+4. I'm really happy with this project overall as I'm starting to feel more confident being able to write less code to perform the same function.
+
+
+![Home page for password generator](./assets/images/work-day.JPG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,27 @@
 // 1. https://api.jquery.com/attr/
 // 2. https://api.jquery.com/siblings/#siblings-selector
 // 3. https://api.jquery.com/val/#val
+// 4. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
 // Grab date to put at top of page
 
-let todayDate = $('#todayDate');
-let momentDate =  moment().format('MMMM Do YYYY, h:mm a');
-
 // Append date to top of page
 
-todayDate.append(momentDate);
+setInterval(function() {
+
+    let todayDate = $('#todayDate');
+    let momentDate =  moment().format('MMMM Do YYYY, h:mm:ss a');
+    todayDate.html(momentDate);
+
+});
 
 // Get current hours
 
-//let timeZ = new Date().getHours();
+let thisHour = new Date().getHours(); 
 
 // Used for testing times outside daily hours
 
- let timeZ = 12;
+//let thisHour = 12;
 
 // Hours array
 
@@ -28,24 +32,22 @@ let allHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 function changeColors() {
 
-$('.todo').each(function(i) {
+    $('.todo').each(function(i) {
+        if (allHours[i] < thisHour) {
+            $(this).addClass('past');
+        } else if (allHours[i] === thisHour) {
+            $(this).addClass('current');
+        } else {
+            $(this).addClass('future')
+        }
+    });
 
-    if (allHours[i] < timeZ) {
-        $(this).addClass('past');
-    } else if (allHours[i] === timeZ) {
-        $(this).addClass('current');
-    } else {
-        $(this).addClass('future')
-    }
-});
 }
 
 changeColors();
 
 // Function to save text to local storage
-
-// REFERENCES
-// 1, 2, and 3
+// REFERENCE - 1, 2, and 3
 
 $('.save-button').on('click', function() {
 
@@ -54,6 +56,21 @@ $('.save-button').on('click', function() {
     localStorage.setItem(saveTime, saveTodo);
 
 });
+
+// Load from local storage when page refreshes
+// Uses the value of the key from local storage called '________' and put it in the ID on the page 
+// REFERENCE - 4
+
+ $('.todo').each(function() {
+
+    grabTodo = $(this).attr('id');
+    $(`#${grabTodo}`).val(localStorage.getItem(`${grabTodo}`));
+
+});
+
+
+
+
 
 
 
